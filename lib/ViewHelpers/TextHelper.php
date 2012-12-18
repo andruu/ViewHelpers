db32a5972cc84be294a9b1bb76b6c88f958dbf14
andruu <andru.weir@gmail.com>
2012-12-17 22:20:25 -0500

    Adding scaffold for more helpers

---
+++ lib/ViewHelpers/TextHelper.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the ViewHelpers package.
+ *
+ * (c) Andrew Weir <andru.weir@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ViewHelpers;
+
+/**
+ * Text Helper
+ *
+ * @package eGloo/ViewHelpers
+ * @author Andrew Weir <andru.weir@gmail.com>
+ **/
+class TextHelper {
+
+}
